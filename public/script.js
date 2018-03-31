75870a3a1f2a4e9998dce0fb2924bd6f5ee527b6
Giuseppe Caruso <giuseppe@bonsai-studio.net>
2018-04-01 00:42:02 +0200

    Project 1 - Building the product list

--- public/script.js
+++ public/script.js
@@ -1,16 +1,65 @@
+const PRICE = 9.99;
+
 new Vue({
     el: '#app',
     data: {
         total: 0,
-        items: [
-            { title: 'Item 1' },
-            { title: 'Item 2' },
-            { title: 'Item 3' },
-        ]
+        items: [],
+        cart: [],
+        newSearch: 'blondes',
+        lastSearch: '',
+        loading: false,
+        price: PRICE
     },
     methods: {
-        addItem() {
-            this.total += 42.42;
+        onSubmit() {
+            this.items = [];
+            this.loading = true;
+            this.$http.get(`/search/${this.newSearch}`)
+            .then(function (result) {
+                this.lastSearch = this.newSearch;
+                this.items = result.data;
+                this.loading = false;
+            })
+        },
+        addItem(index) {
+            this.total += PRICE;
+            let found = false;
+            let itemInCart = this.cart.find(item => item.id === this.items[index].id);
+
+            if (itemInCart) {
+                itemInCart.qty++;
+            } else {
+                this.cart.push({
+                    id: this.items[index].id,
+                    title: this.items[index].title,
+                    price: PRICE,
+                    qty: 1
+                });
+            }
+        },
+        inc(item) {
+            item.qty++;
+            this.total += PRICE;
+        },
+        dec(item) {
+            item.qty--;
+            this.total -= PRICE;
+            if(item.qty === 0) {
+                this.cart.forEach((product, index) => {
+                   if(product.id === item.id) {
+                        this.cart.splice(index, 1);
+                        return;
+                   }
+                });
+            }
         }
+    },
+    filters: { currency(price) {
+            return `$${price.toFixed(2)}`;
+        }
+    },
+    mounted() {
+        this.onSubmit()
     }
 });
